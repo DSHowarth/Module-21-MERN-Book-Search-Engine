513af49b20e8d4dba5bac0fd341c562cfe60be95
Dash <dkshowarth@gmail.com>
2023-09-04 13:24:47 -0700

    add book, user, auth typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const typeDefs =`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Number
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`
