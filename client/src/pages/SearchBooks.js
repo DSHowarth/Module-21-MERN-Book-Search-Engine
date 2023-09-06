30cd5f1b01aafaf639fd9a00cc3ca3419604d7cf
Dash <dkshowarth@gmail.com>
2023-09-05 20:23:16 -0700

    add save new book functionality to front end

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -11,8 +11,12 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation} from '@apollo/client';
+import SAVE_BOOK from '../utils/mutations';
 
 const SearchBooks = () => {
+
+  const [saveNewBook, {data, loading, error}] = useMutation(SAVE_BOOK)
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -70,19 +74,30 @@ const SearchBooks = () => {
     if (!token) {
       return false;
     }
-
+    
     try {
-      const response = await saveBook(bookToSave, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveNewBook(bookToSave);
 
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
+    } catch {
+      console.error(error);
     }
+
+    // try {
+    //   const response = await saveBook(bookToSave, token);
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   // if book successfully saves to user's account, save book id to state
+    //   setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    // } catch (err) {
+    //   console.error(err);
+    // }
+
+
   };
 
   return (
