6ae41eda209bec6ea66a63f35dca0df53c0c8695
Dash <dkshowarth@gmail.com>
2023-09-06 18:42:30 -0700

    potential fix for heroku deploy error ENOENT

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Module-21-MERN-Book-Search-Engine
+
+## Description
+
+## Usage
+
+## Technologies Used
+
+## Lessons Learned
+
+## Credits
+- Starter code courtesy of https://github.com/coding-boot-camp/solid-broccoli
--- server/server.js
+++ server/server.js
@@ -36,10 +36,10 @@ const startApolloServer = async () => {
 
   // if server is spun up in a production environment, it needs new path names for associated files.
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
   }
 
